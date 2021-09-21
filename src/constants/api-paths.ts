6112582c811c992f485d32459d67f7d13fb077ef
katyachok <katetestwo@outlook.com>
2021-09-21 21:20:30 +0300

    update import path

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
 	product: 'https://y4wh5s1bx8.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	import: 'https://hs14os3p5d.execute-api.eu-west-1.amazonaws.com/dev',
 	bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	products: 'https://y4wh5s1bx8.execute-api.eu-west-1.amazonaws.com/dev',
